e45b6e630629265bb40d11428f3bd50edf98e8ea
Jami Couch <fbcouch@gmail.com>
2013-10-15 09:52:35 -0500

    Added script to compile gh pages

---
+++ compile-to-gh-pages.sh
@@ -0,0 +1,6 @@
+coffee -o out -mc src/*.coffee
+cp index.html ../space-domination-coffee-gh-pages/
+cp -R assets ../space-domination-coffee-gh-pages/
+cp -R css ../space-domination-coffee-gh-pages/
+cp -R lib ../space-domination-coffee-gh-pages/
+cp -R out ../space-domination-coffee-gh-pages/
